9a160468973a56b0a1f850c91fd7466473d90750
Michael Lutz <michel.lutz92@gmail.com>
2026-05-07 11:27:17 +0200

    Implement card flipping functionality and add next card feature with dynamic content updates

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+let mouseX = 0;
+let mouseY = 0;
+
+const imgs = ["knight.png", "hero.png","goblins.png"];
+const titles = ["Gefallener Ritter", "Unerschrockener Held", "Goblin Armee"];
+const texts = [
+    "Ein gefallener Ritter aus den Schattenkriegen des Hochmittelalters, dessen Seele durch endlose Schlachten verhärtet wurde und dessen Klinge unzählige Feinde niedergestreckt hat.",
+    "Ein unerschrockener Held, der sich den Mächten der Dunkelheit entgegenstellt und das Schicksal der Welt in seinen Händen hält.",
+    "Eine Goblin Armee, die aus den Tiefen der Wälder aufsteigt, um Chaos und Zerstörung zu verbreiten."
+];
+
+const karte = document.getElementById("karte001")
+const titel = karte.getElementsByTagName("div")[0].getElementsByTagName("span")[0];
+const bild = karte.getElementsByTagName("div")[0].getElementsByTagName("img")[0];
+const text = karte.getElementsByTagName("div")[0].getElementsByTagName("p")[0];
+
+
 document.getElementById("karte001").classList.add("flipFrontStep2");
 function flip(card){
     if (card.classList.contains("flipFrontStep2")) {
@@ -19,7 +36,28 @@ function flip(card){
             card.getElementsByClassName("front")[0].style.display = "none";
             card.classList.remove("flipBackStep1");
             card.classList.add("flipBackStep2");
+            
         }
     }, 200);
-
 }
+
+let counter = 0;
+let maxCounter = titles.length;
+function nextCard(){
+    counter = counter + 1
+    if(counter >= maxCounter){
+        counter = 0;
+    }
+
+    titel.textContent = titles[counter];
+    bild.src = "./imgs/" + imgs[counter];
+    text.textContent = texts[counter];
+}
+
+document.addEventListener("click", function(event) {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+    karte.style.left = (mouseX-(karte.offsetWidth)/2)+"px";
+    karte.style.top = (mouseY-(karte.offsetHeight)/2)+"px";
+
+} );
